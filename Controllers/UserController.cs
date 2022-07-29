0fe35b5944e085b5cc7a05cd28b8cd3df4cc3714
DHRUVESH PANCHAL <dhruveshpanchal36@gmail.com>
2022-07-29 23:18:00 +0530

    added user controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bank_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("working");
+        }
+
+
+    }
+}
