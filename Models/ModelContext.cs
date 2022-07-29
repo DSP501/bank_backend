238d6614b569e3d3f69a27ab50ad8a892b0cf432
DHRUVESH PANCHAL <dhruveshpanchal36@gmail.com>
2022-07-29 23:06:13 +0530

    inital commit

---
+++ Models/ModelContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bank_backend.Models
+{
+    public class ModelContext : DbContext
+    {
+        public ModelContext(DbContextOptions<ModelContext> options)
+            : base(options)
+        {
+
+        }
+
+        // public DbSet<<Model_Name>> <Table_name> { get; set; }
+    }
+}
